a74c74bc88bce1a08a1f871ac61237b6f5a2143c
Abduliokhanio <akhan17@outlook.com>
2025-09-21 14:25:51 -0500

    eslint test 3

---
+++ src/applications/health-care-hello-world/components/GeneralsVsComponent.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function GeneralsVsComponent({ element }) {
+  return (
+    <va-card>
+      <p>
+        <b>Confederate Commander</b> - {element.attributes.CSACom}
+      </p>
+      <div style={{ textAlign: 'center' }}>
+        <b>vs</b>
+      </div>
+      <p>
+        <b>States Commander</b> - {element.attributes.USACom}
+      </p>
+    </va-card>
+  );
+}
